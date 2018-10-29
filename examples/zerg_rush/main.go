83724c7b1080a68551701fc09ef0837beefa1b85
AiSee <aiseeq@gmail.com>
2018-10-29 22:44:32 +0200

    zerg rush

---
+++ examples/zerg_rush/main.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"fmt"
+	"github.com/chippydip/go-sc2ai/api"
+	abilityType "github.com/chippydip/go-sc2ai/api/ability"
+	"time"
+
+	// using unit.Protoss_Nexus is not convenient, because everywhere you wish to iterate []*Units as unit
+	unitType "github.com/chippydip/go-sc2ai/api/unit"
+	"github.com/chippydip/go-sc2ai/client"
+	"github.com/chippydip/go-sc2ai/runner"
+	"math/rand"
+)
+
+type zergRush struct {
+	client.AgentInfo
+
+	actions []*api.Action
+
+	myStartLocation    *Point
+	enemyStartLocation *Point
+	observation        *api.Observation
+	units              map[unitType.Unit][]*api.Unit
+	neutralUnits       map[unitType.Unit][]*api.Unit
+	enemyUnits         map[unitType.Unit][]*api.Unit
+
+	typeData       []*api.UnitTypeData // actually, a map[unitType.Unit]
+	unitAttributes map[unitType.Unit]map[api.Attribute]bool
+
+	okTargets   []*api.Unit
+	goodTargets []*api.Unit
+
+	minerals uint32
+	vespene  uint32
+	foodCap  int
+	foodUsed int
+	foodLeft int
+}
+
+func New() *zergRush {
+	bot := new(zergRush)
+	bot.unitAttributes = map[unitType.Unit]map[api.Attribute]bool{}
+	return bot
+}
+
+func (bot *zergRush) parseUnits() {
+	bot.units = map[unitType.Unit][]*api.Unit{}
+	bot.neutralUnits = map[unitType.Unit][]*api.Unit{}
+	bot.enemyUnits = map[unitType.Unit][]*api.Unit{}
+	for _, unit := range bot.AgentInfo.Observation().GetObservation().GetRawData().GetUnits() {
+		switch unit.Alliance {
+		case api.Alliance_Self:
+			bot.units[unit.UnitType] = append(bot.units[unit.UnitType], unit)
+		case api.Alliance_Enemy:
+			bot.enemyUnits[unit.UnitType] = append(bot.enemyUnits[unit.UnitType], unit)
+		case api.Alliance_Neutral:
+			bot.neutralUnits[unit.UnitType] = append(bot.neutralUnits[unit.UnitType], unit)
+		default:
+			fmt.Println("Not supported alliance: ", unit)
+		}
+	}
+}
+
+// OnGameStart is called once at the start of the game
+func (bot *zergRush) OnGameStart(info client.AgentInfo) {
+	bot.AgentInfo = info
+	bot.parseUnits()
+	bot.typeData = bot.AgentInfo.Data().GetUnits()
+	for _, utd := range bot.typeData {
+		for _, attribute := range utd.Attributes {
+			bot.unitAttributes[utd.UnitId] = map[api.Attribute]bool{}
+			bot.unitAttributes[utd.UnitId][attribute] = true
+		}
+	}
+
+	// My hatchery is on start position
+	bot.myStartLocation = PointFrom3D(bot.units[unitType.Zerg_Hatchery][0].Pos)
+	bot.enemyStartLocation = PointFrom2D(bot.AgentInfo.GameInfo().GetStartRaw().GetStartLocations()[0])
+
+	// Send a friendly hello
+	bot.ChatSend("(glhf)")
+}
+
+func (bot *zergRush) ParseObservation() {
+	bot.observation = bot.AgentInfo.Observation().GetObservation()
+	bot.parseUnits()
+
+	bot.okTargets = nil
+	bot.goodTargets = nil
+	for _, units := range bot.enemyUnits {
+		for _, unit := range units {
+			if !unit.IsFlying && unit.UnitType != unitType.Zerg_Larva && unit.UnitType != unitType.Zerg_Egg {
+				bot.okTargets = append(bot.okTargets, unit)
+				if !bot.unitAttributes[unit.UnitType][api.Attribute_Structure] {
+					bot.goodTargets = append(bot.goodTargets, unit)
+				}
+			}
+		}
+	}
+
+	bot.minerals = bot.observation.PlayerCommon.Minerals
+	bot.vespene = bot.observation.PlayerCommon.Vespene
+	bot.foodCap = int(bot.observation.PlayerCommon.FoodCap)
+	bot.foodUsed = int(bot.observation.PlayerCommon.FoodUsed)
+	bot.foodLeft = bot.foodCap - bot.foodUsed
+}
+
+func (bot *zergRush) AlreadyTraining(ability abilityType.Ability) int {
+	count := 0
+	units := bot.units[unitType.Zerg_Egg]
+	if ability == abilityType.Train_Queen {
+		units = bot.units[unitType.Zerg_Hatchery]
+	}
+	for _, unit := range units {
+		if len(unit.Orders) > 0 && unit.Orders[0].AbilityId == ability {
+			count++
+		}
+	}
+	return count
+}
+
+func (bot *zergRush) Strategy() {
+	// Wait until we have enough minerals to build spawning pool
+	if bot.minerals >= 200 && len(bot.units[unitType.Zerg_SpawningPool]) == 0 &&
+		len(bot.units[unitType.Zerg_Drone]) > 0 {
+		builder := bot.units[unitType.Zerg_Drone][0]
+		dist := bot.enemyStartLocation.Distance(bot.myStartLocation)
+		vec := bot.enemyStartLocation.Sub(bot.myStartLocation).Div(&Point{dist, dist})
+		pos := bot.myStartLocation.Add(vec.Mul(&Point{5, 5}))
+		bot.unitCommandTargetPos(builder, abilityType.Build_SpawningPool, pos)
+		return
+	}
+	// We are building spawning pool (or it is ready)
+	if len(bot.units[unitType.Zerg_SpawningPool]) > 0 {
+		// Build drones
+		if len(bot.units[unitType.Zerg_Drone]) < 14 && len(bot.units[unitType.Zerg_Larva]) > 0 && bot.minerals >= 50 {
+			bot.unitCommand(bot.units[unitType.Zerg_Larva][0], abilityType.Train_Drone)
+			return
+		}
+		// Build overlords
+		if bot.foodLeft < 2 && len(bot.units[unitType.Zerg_Larva]) > 0 && bot.minerals >= 100 &&
+			bot.AlreadyTraining(abilityType.Train_Overlord) == 0 {
+			bot.unitCommand(bot.units[unitType.Zerg_Larva][0], abilityType.Train_Overlord)
+			return
+		}
+		// If pool is ready
+		if bot.units[unitType.Zerg_SpawningPool][0].BuildProgress == 1 {
+			// Build zerglings
+			if len(bot.units[unitType.Zerg_Larva]) > 0 && bot.minerals >= 50 {
+				bot.unitCommand(bot.units[unitType.Zerg_Larva][0], abilityType.Train_Zergling)
+				return
+			}
+			if len(bot.units[unitType.Zerg_Queen]) == 0 && bot.minerals >= 150 &&
+				bot.AlreadyTraining(abilityType.Train_Queen) == 0 && len(bot.units[unitType.Zerg_Hatchery]) > 0 {
+				bot.unitCommand(bot.units[unitType.Zerg_Hatchery][0], abilityType.Train_Queen)
+				return
+			}
+		}
+	}
+}
+
+func (bot *zergRush) Tactics() {
+	lings := bot.units[unitType.Zerg_Zergling]
+	if len(lings) >= 6 {
+		if len(bot.okTargets) == 0 {
+			bot.unitsCommandTargetPos(lings, abilityType.Attack, bot.enemyStartLocation)
+		} else {
+			// To see battle better
+			if len(bot.goodTargets) > 0 {
+				time.Sleep(time.Millisecond * 20)
+			}
+			for _, ling := range lings {
+				if len(bot.goodTargets) > 0 {
+					target := PointFrom3D(ling.Pos).ClosestUnit(bot.goodTargets)
+					if PointFrom3D(ling.Pos).Distance(PointFrom3D(target.Pos)) > 4 {
+						// If target is far, attack it as unit, ling will run ignoring everything else
+						bot.unitCommandTargetTag(ling, abilityType.Attack, target.Tag)
+					} else {
+						// Attack as position, ling will choose best target around
+						bot.unitCommandTargetPos(ling, abilityType.Attack, PointFrom3D(target.Pos))
+					}
+				} else {
+					target := PointFrom3D(ling.Pos).ClosestUnit(bot.okTargets)
+					bot.unitCommandTargetPos(ling, abilityType.Attack, PointFrom3D(target.Pos))
+				}
+			}
+		}
+	}
+
+	queens := bot.units[unitType.Zerg_Queen]
+	// If queen can inject, do it
+	if len(queens) > 0 && queens[0].Energy >= 25 && len(bot.units[unitType.Zerg_Hatchery]) > 0 {
+		bot.unitCommandTargetTag(queens[0], abilityType.Effect_InjectLarva, bot.units[unitType.Zerg_Hatchery][0].Tag)
+	}
+}
+
+// OnStep is called each game step (every game update by defaul)
+func (bot *zergRush) OnStep() {
+	bot.ParseObservation()
+
+	bot.Strategy()
+	bot.Tactics()
+
+	if len(bot.actions) > 0 {
+		bot.SendActions(bot.actions)
+		bot.actions = nil
+	}
+}
+
+// OnGameEnd is called once the game has ended
+func (bot *zergRush) OnGameEnd() {
+	bot.ChatSend("(gg)")
+}
+
+func main() {
+	maps := []string{"AcidPlantLE", "BlueshiftLE", "CeruleanFallLE", "DreamcatcherLE",
+		"FractureLE", "LostAndFoundLE", "ParaSiteLE"}
+
+	rand.Seed(time.Now().UnixNano())
+	runner.Set("map", maps[rand.Intn(len(maps))]+".SC2Map")
+	runner.Set("ComputerOpponent", "true")
+	runner.Set("ComputerRace", "random")
+	runner.Set("ComputerDifficulty", "Medium")
+
+	// Create the agent and then start the game
+	runner.RunAgent(client.NewParticipant(api.Race_Zerg, New()))
+}
---
+++ examples/zerg_rush/points.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/chippydip/go-sc2ai/api"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+func PointFrom2D(a *api.Point2D) *Point {
+	return &Point{X: float64(a.X), Y: float64(a.Y)}
+}
+
+func PointFrom3D(a *api.Point) *Point {
+	return &Point{X: float64(a.X), Y: float64(a.Y)}
+}
+
+func (a *Point) To2D() *api.Point2D {
+	return &api.Point2D{X: float32(a.X), Y: float32(a.Y)}
+}
+
+func (a *Point) Add(b *Point) *Point {
+	return &Point{X: a.X + b.X, Y: a.Y + b.Y}
+}
+
+func (a *Point) Sub(b *Point) *Point {
+	return &Point{X: a.X - b.X, Y: a.Y - b.Y}
+}
+
+func (a *Point) Mul(b *Point) *Point {
+	return &Point{X: a.X * b.X, Y: a.Y * b.Y}
+}
+
+func (a *Point) Div(b *Point) *Point {
+	return &Point{X: a.X / b.X, Y: a.Y / b.Y}
+}
+
+func (a *Point) Distance(b *Point) float64 {
+	return math.Sqrt(a.DistanceSquared(b))
+}
+
+func (a *Point) DistanceSquared(b *Point) float64 {
+	return math.Pow(b.X-a.X, 2) + math.Pow(b.Y-a.Y, 2)
+}
+
+// User should check that he receives not nil
+func (a *Point) ClosestUnit(units []*api.Unit) *api.Unit {
+	var closest *api.Unit = nil
+	for _, unit := range units {
+		if closest == nil ||
+			(a.DistanceSquared(PointFrom3D(closest.Pos)) > a.DistanceSquared(PointFrom3D(unit.Pos))) {
+			closest = unit
+		}
+	}
+	return closest
+}
