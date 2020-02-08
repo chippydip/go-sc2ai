f7a26709a2a4e148406499f610e0b2dde442d0f3
Chip Bradford <cfbradford@gmail.com>
2020-02-07 22:55:07 -0800

    update test bot

--- examples/search_test/main.go
+++ examples/search_test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/chippydip/go-sc2ai/api"
 	"github.com/chippydip/go-sc2ai/botutil"
@@ -16,10 +17,10 @@ type bot struct {
 
 func main() {
 	// Play a random map against an easy difficulty computer
-	runner.SetMap(runner.Random1v1Map())
 	runner.SetComputer(api.Race_Random, api.Difficulty_Easy, api.AIBuild_RandomBuild)
 
 	// Create the agent and then start the game
+	botutil.SetGameVersion()
 	agent := client.AgentFunc(runAgent)
 	runner.RunAgent(client.NewParticipant(api.Race_Protoss, agent, "SearchTest"))
 }
@@ -30,11 +31,22 @@ func runAgent(info client.AgentInfo) {
 	bot.SetPerfInterval(224)
 
 	search.CalculateBaseLocations(bot.Bot, true)
+	pg := search.NewPlacementGrid(bot.Bot)
 
 	for bot.IsInGame() {
+		pg.DebugPrint(bot.Bot)
+
 		if err := bot.Step(1); err != nil {
 			log.Print(err)
 			break
 		}
+
+		if bot.GameLoop > 224*30 {
+			break
+		}
+	}
+
+	for {
+		time.Sleep(time.Second)
 	}
 }
